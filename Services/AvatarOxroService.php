aa0cdfd9bba6f099a54cc55c3f9c4da9340c662c
gtko <gtko@users.noreply.github.com>
2021-11-07 15:49:26 +0100

    Fix avatar

--- Services/AvatarOxroService.php
+++ Services/AvatarOxroService.php
@@ -6,6 +6,9 @@ namespace Modules\AvatarOxro\Services;
 
 use Modules\BaseCore\Contracts\Services\AvatarContract;
 
+/**
+ * Connection a l'api avatar oxro
+ */
 class AvatarOxroService implements AvatarContract
 {
 
